455d4a565e579c9c956a3bddc9291717a4f8c0ec
Yuri Rublev <rubleviura34@hotmail.com>
2026-06-05 21:10:05 +0300

    Update CachyOS UEFI.m

--- CachyOS UEFI.m
+++ CachyOS UEFI.m
@@ -1076,6 +1076,17 @@ sed -i 's|^GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 
 sed -i "s|^GRUB_CMDLINE_LINUX=.*|GRUB_CMDLINE_LINUX=\"resume=UUID=${SWAP_UUID}\"|" /etc/default/grub
 # 📋 Настройка отображения Grub
 sed -i 's|^#*GRUB_DISTRIBUTOR=.*|GRUB_DISTRIBUTOR="CachyOS"|' /etc/default/grub
+# 📋 Добавление пунктов меню включение и перезагрузка системы 
+sudo tee -a /etc/grub.d/40_custom << 'EOF'
+
+menuentry "Выключение системы" {
+    halt
+}
+
+menuentry "Перезагрузка системы" {
+    reboot
+}
+EOF
 # #------------------------------------------------------------------------------
 # ШАГ 5: НАСТРОЙКА GRUB-BTRFS И ГЕНЕРАЦИЯ
 # #------------------------------------------------------------------------------
